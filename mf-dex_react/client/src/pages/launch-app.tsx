a182dc6e49cd492825f3b64657a557d25184ce0b
mostlyfutures <mostlyfutures@gmail.com>
2025-10-15 23:34:49 -0500

    .

--- mf-dex_react/client/src/pages/launch-app.tsx
+++
@@ -1,9 +0,0 @@
-export default function LaunchApp() {
-  return (
-    <div className="fixed inset-0 z-50 bg-primary flex items-center justify-center">
-      <h1 className="text-6xl font-bold text-primary-foreground" data-testid="text-launch-app-title">
-        Launch App
-      </h1>
-    </div>
-  );
-}
